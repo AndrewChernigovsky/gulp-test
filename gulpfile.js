de26de6be02a36698926a0e9fa1fff8810274f9c
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-10-21 17:29:27 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,87 @@
 const gulp = require('gulp');
-const pug = require('gulp-pug');
-const plumber = require('gulp-plumber')
-const sass = require('gulp-sass')(require('sass'));
-const autoprefixer = require('gulp-autoprefixer')
-const cssmin = require('gulp-clean-css')
+const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel');
-const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const pug = require('gulp-pug');
 const del = require('del');
+const cssmin = require('gulp-clean-css');
+const sass = require('gulp-sass')(require('sass'));
+const uglify = require('gulp-uglify');
+const babel = require('gulp-babel');
+const htmlmin = require('gulp-htmlmin');
+const browserSync = require('browser-sync').create();
 
-function pugtohtml() {
-    return gulp.src('build/pages/*.pug')
-    .pipe(plumber())
-    .pipe(pug({pretty: true}))
-    .pipe(plumber.stop())
-    .pipe(gulp.dest('source/'));
-  }
-  
-function scsstoCss() {
-  return gulp.src('build/static/styles/style.scss')
+function clean() {
+  return del('production');
+}
+
+function pug2html() {
+  return gulp.src('source/pug/pages/*.pug')
+  .pipe(plumber())
+  .pipe(pug({pretty: true}))
+  .pipe(plumber.stop())
+  .pipe(gulp.dest('production/'));
+}
+
+function html() {
+  return gulp.src("production/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest("production/"));
+}
+
+
+function scss2css() {
+  return gulp.src('source/sass/styles.scss')
   .pipe(plumber())
   .pipe(sass())
   .pipe(sourcemaps.init())
-  .pipe(cssmin())
   .pipe(autoprefixer())
+  .pipe(cssmin())
   .pipe(plumber.stop())
   .pipe(sourcemaps.write('../css/sourcemaps/'))
-  .pipe(gulp.dest('source/css/'));
+  .pipe(gulp.dest('production/css/'));
 }
 
 function script() {
-  return gulp.src('build/static/js/main.js')
+  return gulp.src('source/js/main.js')
   .pipe(sourcemaps.init())
   .pipe(babel({
       presets: ['@babel/env']
   }))
   .pipe(uglify())
   .pipe(sourcemaps.write('../js/sourcemaps/'))
-  .pipe(gulp.dest('source/js/'));
+  .pipe(gulp.dest('production/js/'));
 }
 
-// function clean() {
-//   return 
-//     del('build')
-// }
-   
+function imageMin() {
+  return gulp.src("source/image/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+  ]))
+  .pipe(gulp.dest("production/image"))
+}
 
-exports.default = gulp.series(pugtohtml, scsstoCss, script);
+function copyImages() {
+  return gulp.src("source/image/**/*.{png,jpg,svg}")
+  .pipe(gulp.dest("production/image"))
+}
+
+
+function createWebp() {
+  return gulp.src("source/image/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("production/image"))
+}
+
+function sprite () {
+  return gulp.src("source/image/icons/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("production/image"));
+}
+
+exports.default = gulp.series(clean, pug2html, html, scss2css, script);
--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,6 @@
 "use strict";var a=6;
 //# sourceMappingURL=sourcemaps/main.js.map
+
+const {hello} = require('./helpers/swiper');
+
+hello();
